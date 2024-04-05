cbf59c9fc9c6bf4f3a1551a9468beb248d3f6505
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-04-04 22:00:11 -0600

    I can't really remember everything I did. Still not fully working but its not a total joke anymore.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ const formModal = document.getElementById('formModal');
 const addBtnModal = document.getElementById('addBtnModal')
 const closeBtn = document.getElementById('closeBtn');
 
+const toDoEl = document.getElementById('todo-cards');
+const inProgEl = document.getElementById('in-progress-cards');
+const doneEl = document.getElementById('done-cards');
+
+
 
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
@@ -11,16 +16,16 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let projectId = '';
+    let taskId = '';
 
     for (let i = 0; i < 3 ; i++) {
-     projectId += characters.charAt(Math.floor(Math.random() * characters.length));
+     taskId += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
-    const timestamp = dayjs().toString(36);
-    projectId += timestamp;
+    const timestamp = dayjs();
+    taskId += timestamp;
 
-    return projectId;
+    return taskId;
 }
 
 const uniqueId = generateTaskId();
@@ -28,8 +33,9 @@ console.log(uniqueId);
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    console.log('create card');
 
-    const taskCard = $('<div>')
+    const taskCard = $(`<div>`)
         .addClass('card project-card draggable my-3 task-card')
         .attr('data-project-id', task.id);
 
@@ -44,9 +50,9 @@ function createTaskCard(task) {
     
     deleteBtn.on('click', handleDeleteTask);
 
-    if (project.dueDate && project.status !== 'done') {
+    if (task.dueDate && task.status !== 'done') {
         const now = dayjs();
-        const taskDueDate = dayjs(project.dueDate, 'DD/MM/YYYY');
+        const taskDueDate = dayjs(task.dueDate, 'MM/DD/YYYY');
     
         if (now.isSame(taskDueDate, 'day')) {
         taskCard.addClass('bg-warning text-white');
@@ -56,40 +62,90 @@ function createTaskCard(task) {
         }
     }
 
+    
     taskCard.append(taskHeader);
     taskCard.append(taskBody);
     taskCard.append(taskDescription);
     taskCard.append(taskDueDate);
     taskCard.append(deleteBtn);
-    toDoEl.append(taskCard);
-    }
+
+    return taskCard;
+
+}
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    let tasks = taskList;
 
+    if (!tasksList) {
+        tasks = [];
+    }
+
+    tasks.forEach(task => {
+        console.log(task);
+        const taskCard = createTaskCard(task);
+        taskList.appendChild(taskCard);
+    })
+
+    $('.task-card').draggable();
+
+    return tasks;
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(){
+    // check local sotage for tasks, 
+    // if none make tasks variable empty array else make value pulled form storage parse.
+    // push new object to array.
+    //save array to storage
+    // re render
     formModal.style.display = 'block';
+
+    $('#task-form').on('submit', function (event) {
+        event.preventDefault();
+
+
+        console.log('Submit');
+
+        const taskName = $('#task-name-input').val();
+        const taskType = $('#task-type-input').val();
+        const dueDate = $('#taskDueDate').val();
+
+        console.log(taskName, taskType, dueDate);
+        const task = {
+            id: generateTaskId(),
+            name: taskName,
+            type: taskType,
+            dueDate: dueDate,
+        };
+
+        createTaskCard(task);
+
+        closeModal();
+    })
+
 };
 
-addBtnWindow.addEventListener('click', handleAddTask);
+addBtnModal.addEventListener('click', handleAddTask);
 
 function closeModal() {
     formModal.style.display = 'none';
+
+    $('#task-name-input').val('');
+    $('#task-type-input').val('');
+    $('#taskDueDate').val('');
 };
 
 closeBtn.addEventListener('click', closeModal);
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-    const projectId = $(this).attr('<insert yours>');
-    const projects = readProjectsFromStorage();
+    const taskId = $(this).attr('<insert yours>');
+    const tasks = taskList;
 
-    projects.forEach((project) => {
-    if (project.id === projectId) {
-    projects.splice(projects.indexOf(project), 1);
+    tasks.forEach((task) => {
+    if (task.id === taskId) {
+    tasks.splice(tasks.indexOf(task), 1);
     }
 });
 }
@@ -102,4 +158,10 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
+
+    $('#taskDueDate').datepicker({
+        format: 'mm-dd-yyyy',
+        autoclose: true,
+        todayHighlight: true
+    });
 });
